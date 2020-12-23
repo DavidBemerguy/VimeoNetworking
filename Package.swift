f64316163c0ece93c0c4263f4356c8aacc8496a2
David Bemerguy <david.bemerguy@gmail.com>
2020-12-23 17:57:37 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+    name: "VimeoNetworking",
+    products: [
+        .library(
+            name: "VimeoNetworking",
+            targets: ["VimeoNetworking"]),
+    ],
+    dependencies: [
+        .package(
+            name: "OHHTTPStubs/Swift",
+            url: "https://github.com/AliSoftware/OHHTTPStubs.git",
+            .exact(from: "8.0.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "VimeoNetworking",
+            dependencies: ["OHHTTPStubs/Swift"],
+            path: "VimeoNetworking/Sources")
+    ]
+)
